d410bda7afbd801358f7dd1042480891af0f23d1
saqibbedar <chandio.saqib.m@gmail.com>
2024-10-19 23:15:46 +0500

    refactor: removed duplicate directory 'C++ Revision'

--- C++ Revision/Filing/00_filing.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <fstream>
-
-int main() {
-    std::fstream file("file.txt", std::ios::out | std::ios::app);
-    file << "Hello, world" << std::endl;
-    file.close();
-    return 0;
-}
--- C++ Revision/Filing/01_filing.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <fstream>
-
-int main() {
-    std::fstream file("file.txt", std::ios::out | std::ios::app | std::ios::ate);
-    file << "# file handling data\n" << std::endl;
-    file.close();
-    return 0;
-}
