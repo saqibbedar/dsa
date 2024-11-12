a4208cea21541b3e1bb2f865ae63716500b0ca53
saqibbedar <chandio.saqib.m@gmail.com>
2024-11-13 01:39:04 +0500

    feat: implement push_back & push_front functions

--- LinkedList/LinkedList Implementation from Scratch/main.cpp
+++ LinkedList/LinkedList Implementation from Scratch/main.cpp
@@ -9,18 +9,65 @@ public:
 };
 
 template <typename T>
-class LinkedList{
+class list{
     Node *head, *tail;
     int size;
 
 public:
 
-    LinkedList() : head(nullptr), tail(nullptr) {}
+    list() : size(0), head(nullptr), tail(nullptr) {}
 
-    // Functions similar to those in std::list
-    void push_back(T val);
-    void push_front(T val);
-    void pop_back();
+    void push_back(T val){ // Add new element to the end of the linked list
+
+        Node<T> *temp = new Node<T>(val); // create a new node with given value
+
+        if(tail == nullptr){
+            head = temp;
+            tail = temp;
+        } else {
+            tail->next = temp; // connect new to the tail next
+            tail = temp;
+        }
+
+        ++size;
+    }
+
+    void push_front(T val){ // Add a new element to the beginning of the linked list
+
+        Node<T> *temp = new Node<T>(val);
+
+        if(head == nullptr){ // incase if it is the first element
+            head = temp;
+            tail = temp;
+        } else {
+            temp->next = head;
+            head = temp;
+        }
+
+        ++size;
+    }
+
+    void pop_back() {
+        if(tail == nullptr){
+            throw("List underflow");
+        }
+
+        Node<T> *temp = head; // save the last node to temp
+
+        while (temp != nullptr)
+        {
+            if(temp->next == tail){ // find the node before the tail
+                delete tail;
+                tail = temp;
+                tail->next = nullptr;
+                temp->next = nullptr; // this will also break the loop;
+                break;
+            }
+            temp = temp->next;
+        }
+
+        --size;
+    }
     void pop_front();
     T front();
     T back();
@@ -31,7 +78,7 @@ public:
     void clear();
     void display() const;
 
-    ~LinkedList();
+    ~list();
 };
 
 int main(){
