c5316ee7617bd88fb09db25a0daeb9bffdabe212
saqibbedar <chandio.saqib.m@gmail.com>
2024-12-15 04:31:58 +0500

    feat: add new iterator methods, comments refactor code

--- Data Structures & Algorithms/Iterators/LinkedList/forward_list/main.cpp
+++ Data Structures & Algorithms/Iterators/LinkedList/forward_list/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <forward_list>
 
 namespace my_std{
 
@@ -15,25 +14,26 @@ namespace my_std{
         Node<T> *head, *tail;
         int n; // size of list
 
-        // merge sorts helper functions
-        // get the middle fo list using slow & fast pointers
-        Node<T>* getMiddle(){
-            // start slow and fast with head
-            Node<T> *slow = head;
-            Node<T> *fast = head;
+        // merge sorts helper function
+        // get the middle of a list using slow & fast pointers
+        // Node<T>* getMiddle(){
+        //     // start slow and fast with head
+        //     Node<T> *slow = head;
+        //     Node<T> *fast = head;
 
-            while(slow != nullptr && fast != nullptr){
-                slow = slow->next; // slow will move one step
-                fast = fast->next->next; // move two steps at a time
-            }
+        //     while(slow != nullptr && fast != nullptr){
+        //         slow = slow->next; // slow will move one step
+        //         fast = fast->next->next; // move two steps at a time
+        //     }
 
-            return slow;
-        }
+        //     return slow;
+        // }
 
     public:
         forward_list() : head(nullptr), tail(nullptr), n(0) {}
         ~forward_list(){clear();}
 
+        // push value in a list
         void push_front(T val) {
             Node<T> *new_node = new Node<T>(val);
             if(this->head == nullptr){
@@ -47,6 +47,7 @@ namespace my_std{
             ++this->n;
         }
 
+        // pop first value in list
         void pop_front(){
             if(this->head == nullptr){
                 throw("Error <forward_list> list is empty");
@@ -62,7 +63,8 @@ namespace my_std{
             }
             --this->n;
         }
-
+        
+        // display all values in list 
         void print_list(){
             if(head == nullptr){
                 throw("Error: <forward_list> list is empty");
@@ -75,7 +77,8 @@ namespace my_std{
             }
             std::cout << "]";
         }
-
+        
+        // return top value in list
         T front() const{
             if(this->head == nullptr){
                 throw("Error: List is empty");
@@ -83,6 +86,7 @@ namespace my_std{
             return head->val;
         }
 
+        // return last value in list
         T back() const {
             if(this->head == nullptr){
                 throw("Error: <forward_list-back> List is empty");
@@ -90,14 +94,17 @@ namespace my_std{
             return this->tail->val;
         }
 
+        // return total size of list
         int size() const {
             return this->n;
         }
-
+        
+        // return boolean value, if list is empty or not?
         bool empty() const {
             return n == 0;
         }
 
+        // make list empty
         void clear(){
             Node<T> *temp = this->head;
             while (temp != nullptr)
@@ -110,6 +117,7 @@ namespace my_std{
             this->n = 0;
         }
 
+        // sort list 
         void my_sort() const{
             if(head == nullptr){
                 throw("Error: <sort> List is empty");
@@ -132,50 +140,70 @@ namespace my_std{
         }
 
         // Merge sort: divide and conquer rule
-        void sort() const {
-            // if empty
-            if(head == nullptr){
-                throw("Error: <sort> List is empty");
-            }
+        // void sort() const { 
+        //     // if empty
+        //     if(head == nullptr){
+        //         throw("Error: <sort> List is empty");
+        //     }
 
-            // Step 1: Split the list into two halves
-            Node<T> *middle = getMiddle(); // getMiddle will return middle of list
-            Node<T> *nexOfMiddle = middle->next; // next part of middle
-            middle->next = nullptr; 
+        //     // Step 1: Split the list into two halves
+        //     Node<T> *middle = getMiddle(); // getMiddle will return middle of list
+        //     Node<T> *nexOfMiddle = middle->next; // next part of middle
+        //     middle->next = nullptr; 
 
-            // Step 2: Recursively sort the two halves
+        //     // Step 2: Recursively sort the two halves
 
 
-            // Step 3: Merge the sorted halves
+        //     // Step 3: Merge the sorted halves
 
-        }
+        // }
 
-        // iterator class
+        // iterator class to traverse through a list
         class iterator{
             Node<T> *ptr;
             friend forward_list;
 
         public:
+            // Check if two iterators are equal, if yes true else false & rhs param is the iterator to compare with
             bool operator!=(const iterator &rhs) const {
                 return this->ptr != rhs.ptr;
             }
+            // Dereferences the iterator to access the value & the value pointed to by the iterator
             T operator*() const {
                 return this->ptr->val;
             }
+            // Pre-increment operator to move the iterator to the next element & return incremented iterator
             iterator operator++(){
                 ptr = ptr->next;
                 return *this;
             }
-                
+            // Post-increment operator to move iterator to the next element & return the iterator before incrementing
+            iterator operator++(int){
+                iterator temp;
+                temp.ptr = this->ptr;
+                this->ptr = this->ptr->next;
+                return temp; // return temp pointer
+            }
+            // Checks if two iterators are equal, true if equal else false & rhs is the iterator to compare with.
+            bool operator==(const iterator &rhs) const {
+                return this->ptr != rhs.ptr;
+            }
+            // Accesses the member of the object pointed to by the iterator & return pointer to the value
+            T* operator->(){
+                return &this->ptr->val; // return the address of value
+            }
         };
 
-        // functions for iterator
+        /* Iterator functions */
+
+        // Returns an iterator pointing to the first element in the list
         iterator begin() const{
             iterator temp;
             temp.ptr = this->head; // assign front to the iterator
             return temp;
         }
 
+        // Returns an iterator pointing to the past-the-end element in the list
         iterator end() const{
             iterator temp;
             temp.ptr = nullptr;
@@ -192,14 +220,14 @@ int main()
     {
         my_std::forward_list<int> fl;
 
-        int i = 0;
-        while (i <= 100000)
+        int i = 2;
+        while (i <= 10)
         {
             fl.push_front(i);
-            ++i;
+            i = i+2;
         }
         
-        fl.sort();
+        fl.my_sort();
 
         my_std::forward_list<int>::iterator it;
 
@@ -208,23 +236,6 @@ int main()
         while(it != fl.end()){
             std::cout << *it << " ";
             ++it;
-        }
-
-        fl.clear();
-        std::cout << std::endl;
-        // std::cout << fl.back() << std::endl;
-
-        std::forward_list<int> fl2;
-        fl2.push_front(1);
-        fl2.push_front(2);
-        fl2.push_front(3);
-        fl2.push_front(4);
-
-        fl2.sort();
-
-        std::forward_list<int>::iterator it2;
-        for (it2 = fl2.begin(); it2 != fl2.end(); ++it2){
-            std::cout << *it2 << " ";
         }
     }
     catch(const char* error)
