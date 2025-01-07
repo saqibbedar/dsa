d2702bfe2d1199dea281c57069a4ea00fd637742
saqibbedar <chandio.saqib.m@gmail.com>
2025-01-07 23:46:44 +0500

    feat: remove bugs & add new recursive methods

--- Data Structures & Algorithms/Binary Search Tree/map.cpp
+++ Data Structures & Algorithms/Binary Search Tree/map.cpp
@@ -1,53 +1,67 @@
 // implementing binary search tree using map
 
 #include <iostream>
+#include <utility>
 
 /*
-    Structure of map node:
+    Structure of Tree node:
 
-       mnode
-    ----------
-   |   Parent  |
-    ----------
-   | key | val |
-    ----------
-   |   is_H    |
-    ----------
-   |  H  |  H  |
-    ----------
+           tnode
+    -----------------
+   |      Parent     |
+    -----------------
+   |   Key  |  Value |
+    -----------------
+   |      is_H       |
+    -----------------
+   |  Left  |  Right |
+    -----------------
 
 */
 
+template <typename K, typename V>
 struct mnode
 {
-    int key; // key
-    int val; // value
+    K key; // key
+    V val; // value
     mnode *P; // Parent
     mnode *L; // Left 
     mnode *R; // Right
-    bool is_H; // is dummy head - it will help us in iterator class
+    bool is_H; // is dummy head? it will help us in iterator class
 };
 
+template <typename K, typename V>
 class map
 {
     /*
     Description: Dummy is acting as a nullptr and we are wasting this node but we are trying to implement as STL is implemented, so we will dynamically allocate it in maps constructor
     */
-    mnode *H; // dummy head (nullptr)
+    mnode<K,V> *H; // dummy head (nullptr)
     int n; // size of tree
+    
+    void display_r(mnode<K,V> *ptr){
+        // In-order traversal
+        if(ptr == H){
+            return;
+        } else {
+            std::cout << "Key: " << ptr->key << ", Value: " << ptr->val << std::endl;
+            display_r(ptr->L);
+            display_r(ptr->R);
+        }
+    }
 
 public:
     map(){
-        H = new mnode; // dynamic dummy head node, it will act as nullptr represented with H
+        H = new mnode<K, V>; // dynamic dummy head node, it will act as nullptr represented with H
         // for dummy head, all mnode parts would be assigned with H OR more specifically nullptr
-        H->P = H;
-        H->L = H;
-        H->R = H;
-        H->is_H = true; // for dummy head node it will be true
+        H->P = this->H;
+        H->L = this->H;
+        H->R = this->H;
+        H->is_H = true; // for dummy head node it will be true 
         n = 0; // tree height is zero still
     };
 
-    void insert (const int &key, const int &val){
+    void insert (const K &key, const V &val){
         /*
         Description: In insert function we will handle multiple steps as follows:
             Step 1: first create a new dynamic mnode and assign key to it and set the Left(L), Right(R) pointers and set is_H to false;
@@ -59,7 +73,7 @@ public:
         */
 
         // as we are inserting new node so first create a new mnode dynamically
-        mnode *new_node = new mnode; // new dynamic mnode
+        mnode<K,V> *new_node = new mnode<K,V>; // new dynamic mnode
         new_node->key = key; // assign key to new_nodes key
         new_node->val = val; // assign value to new_nodes val
         new_node->L = H; // assign dummy head to new_nodes left part
@@ -71,8 +85,8 @@ public:
             new_node->P = H;
             H->P = new_node;
         } else { // if not first node then first find the position where to insert the new_node and then insert it to the tree
-            mnode *current = H->P; // start from root_node - Head parent holds the root_node addrress
-            mnode *parent = H; // parent will be the last node where value would be inserted
+            mnode<K,V> *current = H->P; // start from root_node - Head parent holds the root_node addrress
+            mnode<K,V> *parent = H; // parent will be the last node where value would be inserted
 
             // iterate though tree to find to correct position for the insertion
             while (current != H) // this loop will iterate until we reached to dummy head
@@ -80,7 +94,7 @@ public:
                 parent = current; // assign the current node so that parent can have a record of last node
                 if(key == current->key || val == current->val){ // avoid duplicates key or value
                     return; // if value or key already exist then return simply
-                } else if(val < current->val) { // if given value is less than current node then we know we will go left
+                } else if(key < current->key) { // if given key is less than current node then we know we will go left
                     current = current->L; // move current to left side
                 } else { // if value is greater than the given value then current should move to right side
                     current = current->R; // move current to right side
@@ -88,7 +102,7 @@ public:
             }
 
             // Now we have correct position to insert the new node so check where we should add, at left or right side?
-            if(val < parent->val) { // compare val with parent->val if it less than or not? 
+            if(key < parent->key) { // compare key with parent->key if it less than or not? 
                 parent->L = new_node; // assign new_node at parents left side
                 H->L = new_node; 
             } else {
@@ -105,31 +119,38 @@ public:
     int size () const {
         return n;
     }
+    
+    /*      Recursive Functions       */
+
+    // print values: (in-order traversal)
+    void print_r(){ // r denotes to recursive function
+        display_r(H->P);
+    }
 
     class iterator {
-        mnode *ptr;
+        mnode<K,V> *ptr;
         friend map;
 
         public:
             
             bool operator==(const iterator &rhs) const {
-                return (ptr->key == rhs.ptr->key && ptr->val == rhs.ptr->val);
+                return this->ptr == rhs.ptr;
             }
             
             bool operator!=(const iterator &rhs) const {
-                return ptr == rhs.ptr;
+                return this->ptr != rhs.ptr;
             }
             
-            int operator*() const {
-                std::cout << "Key: " << ptr->key << ", Value: " << ptr->val << std::endl;
+            std::pair<int, int> operator*() const {
+                return {ptr->key, ptr->val};
             }
             
-            int* operator->() const {
+            std::pair<int, int>* operator->() const {
                 // return &ptr->key;
+                return &{ptr->key, ptr->val};
             }
-
-            iterator operator++() {
-                mnode *current = this->ptr;  // Set current pointer to temporary pointer
+            iterator& operator++() {
+                mnode<K,V> *current = this->ptr;  // Set current pointer to temporary pointer
 
                 if (current->R->is_H != true) {
                     current = current->R;
@@ -141,7 +162,7 @@ public:
                     this->ptr = current;
                     return *this;
                 } else {
-                    mnode *parent = current->P;
+                    mnode<K,V> *parent = current->P;
 
                     while (current == parent->R && current->P->is_H != true) {
                         current = parent;
@@ -169,17 +190,18 @@ public:
 };
 
 int main() {
-    map m;
+    map<int,int> m;
     m.insert(10, 20);
     m.insert(20, 40);
     m.insert(760, 340);
     m.insert(230, 410);
     m.insert(204, 4330);
 
-    map::iterator it = m.begin();
+    map<int,int>::iterator it = m.begin();
 
     while(it != m.end()){
-        *it;
+        std::pair<int, int> data = *it;
+        std::cout << "Key: " << data.first << ", Value: " << data.second << std::endl;
         ++it;
     }
 
